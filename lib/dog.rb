428425323c60096cc4ae2aa2287b991a0f5b30a7
Brian Birdwell <bbirdwell@gmail.com>
2018-10-26 23:37:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,7 +85,6 @@ class Dog
     sql = <<-SQL 
       UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
-    # binding.pry
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
